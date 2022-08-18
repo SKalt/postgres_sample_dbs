47b94e88bc33c8b9405e8e116058b1c895d9e81b
Automated <actions@users.noreply.github.com>
2022-08-18 20:46:31 +0000

    chore(db/adventureworks): update 2022-08-18

--- sample_dbs/adventureworks/sql/00_schema.ddl.sql
+++ sample_dbs/adventureworks/sql/00_schema.ddl.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.4
--- Dumped by pg_dump version 14.4 (Ubuntu 14.4-1.pgdg20.04+1)
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5 (Ubuntu 14.5-1.pgdg20.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
